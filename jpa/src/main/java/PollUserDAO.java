09b79cb68011b2ea6daa4144bec153ad55e8488c
Jethuestad <57411743+Jethuestad@users.noreply.github.com>
2021-10-06 13:21:59 +0200

    Generic database path

---
+++ jpa/src/main/java/PollUserDAO.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class PollUserDAO {
+
+    public void insertUser(PollUser pollUser) {
+
+    }
+
+    public void updateUser(PollUser pollUser) {
+
+    }
+
+    public void deleteUser(PollUser pollUser) {
+
+    }
+
+    public List<PollUser> getAll() {
+        return null;
+    }
+
+    public PollUser getUser(PollUser pollUser) {
+        return null;
+    }
+
+    public PollUser getUserById(Long id) {
+        return null;
+    }
+}
