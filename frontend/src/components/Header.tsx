1cdadf47553668fd274c576435528496ebaeaee2
Bo Aanes <boaanes@gmail.com>
2021-11-11 18:34:10 +0100

    add auth logic for header and signin

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,19 +1,34 @@
-import { Box, Flex, Heading, Link, Spacer } from '@chakra-ui/react';
+import { Box, Flex, Heading, Link, Spacer, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useAuth } from '../context/AuthContext';
 
-const Header = (): JSX.Element => (
-    <Box px="2rem" bg="white">
-        <Flex pt="60px" pb="1rem">
-            <Link href="/">
-                <Heading color="red.500">Paul Pollsen</Heading>
-            </Link>
-            <Spacer />
-            <Link href="/signup">Sign up</Link>
-            <Link href="/signin" ml="2rem">
-                Sign in
-            </Link>
-        </Flex>
-    </Box>
-);
+const Header = (): JSX.Element => {
+    const authContext = useAuth();
+
+    return (
+        <Box px="2rem" bg="white">
+            <Flex pt="60px" pb="1rem">
+                <Link href="/">
+                    <Heading color="red.500">Paul Pollsen</Heading>
+                </Link>
+                <Spacer />
+                {authContext.isAuthenticated && (
+                    <>
+                        <Text mr="2rem">Signed in as: {authContext.account?.username}</Text>
+                        <Link onClick={authContext.logout}>Sign out</Link>
+                    </>
+                )}
+                {!authContext.isAuthenticated && (
+                    <>
+                        <Link href="/signup">Sign up</Link>
+                        <Link href="/signin" ml="2rem">
+                            Sign in
+                        </Link>
+                    </>
+                )}
+            </Flex>
+        </Box>
+    );
+};
 
 export default Header;
--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,6 +1,6 @@
-import { Button, Center, FormControl, FormLabel, Heading, Input, Link, Spinner, VStack } from '@chakra-ui/react';
+import { Button, Center, FormControl, FormLabel, Heading, Input, Link, Spinner, VStack, Text } from '@chakra-ui/react';
 import React, { useContext, useEffect } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm, useFormState } from 'react-hook-form';
 import { authenticate } from '../api/api';
 import { useAuth } from '../context/AuthContext';
 import { setLocalStorageItem } from '../utils';
@@ -19,59 +19,69 @@ const SignIn = (): JSX.Element => {
         formState: { errors, isSubmitting },
     } = useForm<Inputs>();
 
-    const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
         authContext.login(data.username, data.password);
     };
-
-    useEffect(() => {
-        console.log(authContext);
-    }, [authContext]);
 
     return (
         <Center>
             <Section mt="10%" w="40%">
-                <Heading mb="2rem" color="red.500">
-                    Sign in
-                </Heading>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <VStack spacing="1rem">
-                        <FormControl>
-                            <FormLabel>Brukernavn</FormLabel>
-                            <Input
-                                id="username"
-                                placeholder="username"
-                                {...register('username', {
-                                    required: 'This is required',
-                                })}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel>Password</FormLabel>
-                            <Input
-                                id="password"
-                                placeholder="password"
-                                type="password"
-                                {...register('password', {
-                                    required: 'This is required',
-                                })}
-                            />
-                        </FormControl>
+                {authContext.isAuthenticated && (
+                    <>
                         <Center>
-                            <Button type="submit" colorScheme="red" isLoading={isSubmitting}>
-                                Sign in
-                            </Button>
+                            <Text>You are already signed in: {authContext.account?.name}</Text>
                         </Center>
-                    </VStack>
-                </form>
-                <Center>{authContext.inProgress && <Spinner />}</Center>
-                <Center my="1rem">
-                    <p>or</p>
-                </Center>
-                <Center>
-                    <Link color="red" href="/signup">
-                        Sign up
-                    </Link>
-                </Center>
+                        <Center>
+                            <Button onClick={authContext.logout}>Sign out</Button>
+                        </Center>
+                    </>
+                )}
+                {!authContext.isAuthenticated && (
+                    <>
+                        <Heading mb="2rem" color="red.500">
+                            Sign in
+                        </Heading>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <VStack spacing="1rem">
+                                <FormControl>
+                                    <FormLabel>Brukernavn</FormLabel>
+                                    <Input
+                                        id="username"
+                                        placeholder="username"
+                                        {...register('username', {
+                                            required: 'This is required',
+                                        })}
+                                    />
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input
+                                        id="password"
+                                        placeholder="password"
+                                        type="password"
+                                        {...register('password', {
+                                            required: 'This is required',
+                                        })}
+                                    />
+                                </FormControl>
+                                <Center>
+                                    <Button type="submit" colorScheme="red" isLoading={isSubmitting}>
+                                        Sign in
+                                    </Button>
+                                </Center>
+                            </VStack>
+                        </form>
+                        <Center>{authContext.inProgress && <Spinner />}</Center>
+                        <Center my="1rem">
+                            <p>or</p>
+                        </Center>
+                        <Center>
+                            <Link color="red" href="/signup">
+                                Sign up
+                            </Link>
+                        </Center>
+                    </>
+                )}
             </Section>
         </Center>
     );
