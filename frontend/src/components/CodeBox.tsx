3bf3875a2da08a0da0682739dae05937a3e58227
Bo Aanes <boaanes@gmail.com>
2021-11-16 11:35:31 +0100

    fix create poll button

--- frontend/src/components/CodeBox.tsx
+++ frontend/src/components/CodeBox.tsx
@@ -1,15 +1,23 @@
-import { Button, Center, FormControl, Heading, Input, VStack } from '@chakra-ui/react';
+import { Button, Center, FormControl, Heading, Input, VStack, Text } from '@chakra-ui/react';
 import React, { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router';
+import { useAuth } from '../context/AuthContext';
 import Section from './Section';
 
 const CodeBox = (): JSX.Element => {
     const [code, setCode] = useState<string>('');
     const history = useHistory();
 
+    const authContext = useAuth();
+
     const handleSubmit = async (evt: FormEvent) => {
         evt.preventDefault();
         history.push(`/polls/${code}`);
+    };
+
+    const handleCreate = () => {
+        if (authContext.account) history.push('/create');
+        else history.push('/signin');
     };
 
     return (
@@ -25,12 +33,14 @@ const CodeBox = (): JSX.Element => {
                         </FormControl>
                         <FormControl mt="1rem">
                             <Center>
-                                <Button type="submit" onSubmit={handleSubmit}>
+                                <Button type="submit" colorScheme="red" onSubmit={handleSubmit}>
                                     Go!
                                 </Button>
                             </Center>
                         </FormControl>
                     </form>
+                    <Text>or</Text>
+                    <Button onClick={handleCreate}>Create Poll</Button>
                 </VStack>
             </Section>
         </Center>
